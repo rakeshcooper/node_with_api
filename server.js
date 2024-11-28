db2e367706d806d35592e3c4527a81573f955624
rakeshcooper <rakeshmurali097@gmail.com>
2024-11-28 20:44:59 +0530

    update

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const path = require("path");
 const http = require("http");
 const PORT = process.env.PORT || 5000;
+const products = require("./data/products");
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("content-Type", "text/html");
-  //   res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>Hi, Welcome coder</h1>");
+  //   res.statusCode = 200;
+  //   res.setHeader("content-Type", "text/html");
+
+  if (req.url === "/api/products" && req.method === "GET") {
+    res.writeHead(200, { "content-type": "application/json" });
+    res.write(JSON.stringify(products));
+  } else {
+    res.writeHead(404, { "content-type": "application/json" });
+    res.write(JSON.stringify({ message: "route not found" }));
+  }
+
   res.end();
 });
 
