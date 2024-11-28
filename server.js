1ac4a25b0d66c6e19953cb87d218e21c2d89c610
rakeshcooper <rakeshmurali097@gmail.com>
2024-11-28 20:23:05 +0530

    update

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const path = require("path");
 const http = require("http");
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("content-Type", "text/html");
+  //   res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1>Hi, Welcome coder</h1>");
+  res.end();
+});
+
+server.listen(PORT, () => {
+  console.log("server is running " + PORT);
+});
