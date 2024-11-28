00778713e66ebfd2933d38204c6e1729d667a37b
rakeshcooper <rakeshmurali097@gmail.com>
2024-11-29 02:16:26 +0530

    up

--- server.js
+++ server.js
@@ -5,22 +5,26 @@ const { getProducts, getProduct } = require("./controllers/productControllers");
 // const products = require("./data/products");
 
 const server = http.createServer(async (req, res) => {
-  if (req.url === "/api/products" && req.method === "GET") {
-    await getProducts(req, res);
-    // res.writeHead(200, { "content-type": "application/json" });
-    // res.end(JSON.stringify(products));
-  } else if (
-    req.url.match(/\/api\/products\/([0-9]+)/) &&
-    req.method === "GET"
-  ) {
-    const id = req.url.split("/")[3];
-    await getProduct(req, res, id);
-  } else {
-    res.writeHead(404, { "content-type": "application/json" });
-    res.write(JSON.stringify({ message: "route not found" }));
-  }
+  try {
+    if (req.url === "/api/products" && req.method === "GET") {
+      await getProducts(req, res);
+      // res.writeHead(200, { "content-type": "application/json" });
+      // res.end(JSON.stringify(products));
+    } else if (
+      req.url.match(/\/api\/products\/([0-9]+)/) &&
+      req.method === "GET"
+    ) {
+      const id = req.url.split("/")[3];
+      await getProduct(req, res, id);
+    } else {
+      res.writeHead(404, { "content-type": "application/json" });
+      res.write(JSON.stringify({ message: "route not found" }));
+    }
 
-  res.end();
+    res.end();
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 server.listen(PORT, () => {
