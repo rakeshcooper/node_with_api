f2f680b7f62398d662ab2a8305ea0d2a859d2186
rakeshcooper <rakeshmurali097@gmail.com>
2024-11-30 22:55:57 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const http = require("http");
 const PORT = process.env.PORT || 5000;
-const { getProducts, getProduct, createProduct } = require("./controllers/productControllers");
+const { getProducts, getProduct, createProduct, updateProduct } = require("./controllers/productControllers");
 // const products = require("./data/products");
 
 const server = http.createServer(async (req, res) => {
@@ -13,6 +13,9 @@ const server = http.createServer(async (req, res) => {
       await getProduct(req, res, id);
     } else if(req.url === "/api/products" && req.method === "POST"){
       await createProduct(req, res)
+    }else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === "PUT") {
+      const id = req.url.split("/")[3];
+      await updateProduct(req, res, id);
     } else {
       res.writeHead(404, { "content-type": "application/json" });
       res.write(JSON.stringify({ message: "route not found" }));
